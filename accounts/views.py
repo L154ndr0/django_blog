0db2f7199f9be13539233c08de9805ed912deffe
vaquita_varina34 <vaquitauwu34@gmail.com>
2023-06-03 16:10:08 -0400

    actualizacion

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 # Create your views here.
+
 class SingUpView(CreateView):
     form_class = UserCreationForm
     template_name ='registration/signup.html'
